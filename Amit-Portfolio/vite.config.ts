a0687ecf599a7d1e138cfda1f6530eea48a8ddbe
amit reuveni <amitr123r@gmail.com>
2026-03-09 13:28:50 +0200

    update the deploy option

--- Amit-Portfolio/vite.config.ts
+++ Amit-Portfolio/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Amit-Protofolio/",
+  base: "/Amit-Portfolio/",
   plugins: [
     react({
       babel: {
